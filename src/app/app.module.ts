cb1aaa4a05ea6487e9e675305ef2c26de8b677f6
Sebastian Quiroz <sebastian.122@hotmail.com>
2020-01-02 17:00:36 -0500

    Se crea los componente HEADER, BANNER, FOOTER. Se configura el .css de un API traida de internet y descargada llamada SWIPER (npm install swiper --save) se condigura en el archivo angular.json seccion root:styles. Se implementa el api Swiper inicializandolo en 'home.componente.ts'. Se configura home.component.html agregando los tres coponentes HEADER, BANNER, FOOTER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 // Importaciones adicionales para el proyecto
 import { FormsModule } from '@angular/forms';
 
-// Componente
+// Componentes
 import { ProductComponent } from './components/product.component';
 import { CartComponent } from './cart/cart.component';
 import { ExponentialPipePipe } from './exponential-pipe.pipe';
@@ -16,6 +16,10 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,7 +32,10 @@ import { DemoComponent } from './demo/demo.component';
     HomeComponent,
     ProductsComponent,
     ContactComponent,
-    DemoComponent
+    DemoComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  // se crea un atributo tipo Swiper y se inicializa apuntando al contendor
+  mySwiper: Swiper;
+  ngAfterViewInit(): void {
+    this.mySwiper = new Swiper('.swiper-container');
+  }
 
   constructor() { }
 
