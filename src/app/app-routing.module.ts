7675fe6a1ba59c80ebdc21d86c346ec4141124be
Sebastian Quiroz <sebastian.122@hotmail.com>
2020-01-13 10:11:52 -0500

    Se modifica el componente header.component.html para aplicar 'routerLink y routerLinkActive' en rutas de navegacion, para seguir con la metodologia SPA y no tener que recargar sriempre la pagina, adicionalmente se agregaron sus respectivos scss del header. Ase agregar el componente Header a las componentes, contacts, home y products. Finalmente se agregaron rutas adiconales para la 'no declaracion de ruta' y 'rutas no existentes' (para esta ultima se creo un componente que maneja la pagina 404)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,17 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 // Se agrega cada una de las rutas con su respectivo componente
 const routes: Routes = [
+  {
+    // Ruta cuando no se especifica ruta exacta
+    // redirecciona a /home.
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
   {
     // El simbolo '/' NO DE DEBE PONER '/home'
     path: 'home',
@@ -24,6 +33,11 @@ const routes: Routes = [
   {
     path: 'demo',
     component: DemoComponent
+  },
+  {
+    // Pagina 404, por si el usuario ingresa una ruta no existente
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
