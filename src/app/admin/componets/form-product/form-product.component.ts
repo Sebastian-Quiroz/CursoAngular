4c53b9633d0183fe5849427f8a8a91a5da0f405e
Sebastian Quiroz <sebastian.122@hotmail.com>
2020-01-24 12:55:42 -0500

    Se estiliza mejor el html del formulario para crear un nuevo producto, se agregan temas de MATERIAL, y se agrega la logica para consumir el servicio de CrearProducto (que ya habia sido creado hace mucho). se implementa 'ngSubmit' en el formulario para que envie toda la informacion apenas se de click al boton del formulario que esta espeficiado como tipo 'submit'

--- src/app/admin/componets/form-product/form-product.component.ts
+++ src/app/admin/componets/form-product/form-product.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductsService } from 'src/app/core/services/products/products.service';
+
+// Implementa Router para navidad a una ruta que desemos despues de alguna accion que planeemos
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-product',
@@ -11,8 +15,10 @@ export class FormProductComponent implements OnInit {
   form: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
-  ) { 
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
+  ) {
     this.buildForm();
   }
 
@@ -24,9 +30,25 @@ export class FormProductComponent implements OnInit {
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
-      price:[0, [Validators.required]],
+      price: [0, [Validators.required]],
       image: [''],
       description: ['', [Validators.required]]
     });
+  }
+  // Por buena practica es mejor colocar que se reciba un evento
+  // y asi no permitir que la pagina trate de renderizar por defecto
+  saveProduct(event: Event) {
+    event.preventDefault();
+    // Siempre verificar si el formulario es valido
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productsService.createProduct(product)
+      .subscribe(newProduct => {
+        console.log(newProduct);
+        this.router.navigate(['./admin/products']);
+      });
+    }
+    console.log(this.form.value);
+
   }
 }
